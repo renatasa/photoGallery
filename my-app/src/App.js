19ce66214ae378538fbc58cd1adf5f7e8230b50c
Begemotas <cheminiselementas@gmail.com>
2020-10-25 10:33:58 +0200

    react router dom installed, navlinks added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import Home from './components/Home/Home';
 import Section from './components/Section/Section';
+import {Route, Switch} from 'react-router-dom';
 
 
 export class App extends Component {
   render() {
     return (
       <div>
+        {/* <Switch>
+          <Route path="/people">
+            <People />
+          </Route>
+          
+          <Route path="/landscape">
+            <Landscape />
+          </Route>
+
+          <Route path="/street">
+            <Street/>
+          </Route>
+
+          <Route path="/reportage">
+            <Reportage/>
+          </Route>
+
+          <Route path="/siluets">
+            <Siluets/>
+          </Route>
+
+          <Route path="/travel">
+            <Travel/>
+          </Route>
+
+        </Switch>  */}
+
         <Section/>
       </div>
     )
--- my-app/src/components/Home/Home.js
+++ my-app/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.scss';
+import {NavLink} from 'react-router-dom';
 import portraits from '../../assets/img/large/Portraits.jpg';
 import landscape from '../../assets/img/large/Landscape.jpg';
 import street from '../../assets/img/large/Street.jpg';
@@ -9,7 +10,7 @@ import travel from '../../assets/img/large/Travel.jpg';
 
 
 export const home=()=>{
-        
+
     return (
             <div class="home">
                 thos is home
@@ -21,18 +22,18 @@ export const home=()=>{
                 <div class="home-links">
 
                 <div class="home-sections">
-                 <img class="testImg" src={portraits} alt={portraits}/>
-                 <img class="testImg" src={landscape} alt={landscape}/>
+                <NavLink to="/people"><img class="testImg" src={portraits} alt={portraits}/></NavLink>
+                <NavLink to="/landscape"><img class="testImg" src={landscape} alt={landscape}/> </NavLink>
                 </div>
 
                 <div  class="home-sections">
-                    <img class="testImg" src={street} alt={street}/>
-                    <img class="testImg" src={reportage} alt={reportage}/>
+                <NavLink to="/street"> <img class="testImg" src={street} alt={street}/></NavLink>
+                <NavLink to="/reportage">   <img class="testImg" src={reportage} alt={reportage}/></NavLink>
                 </div>
 
                 <div  class="home-sections">
-                    <img class="testImg" src={siluets} alt={siluets}/>
-                    <img class="testImg" src={travel} alt={travel}/>
+                <NavLink to="/siluets"><img class="testImg" src={siluets} alt={siluets}/></NavLink>
+                <NavLink to="/travel"><img class="testImg" src={travel} alt={travel}/></NavLink>
                 </div>
 
                 </div>
