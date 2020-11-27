26740d99705a617493a5d890e36fe8e7bfd5c4a3
Begemotas <cheminiselementas@gmail.com>
2020-11-27 20:11:18 +0200

    homepage , photo sections page responsivness added

--- my-app/src/components/Slideshow/Slideshow.js
+++ my-app/src/components/Slideshow/Slideshow.js
@@ -6,11 +6,15 @@ export const slideshow=(props)=>{
         return (
                <div>
                <div class="slide">
-                   <span class="close cursor" onClick={props.closeSlide}>&times;</span>
-               
+                   
+                    <div class={!props.show ? "closedImg" : " "}>
+                    <span class="close cursor" onClick={props.closeSlide}>&times;</span>
                     <a class="prev" onClick={props.minusSlide}>&#10094;</a>
-                    <img class={props.photoFade==null || props.photoFade==0 ? "slideImg fade" : "slideImg fade2"}  src={props.currentImg} alt="imgOne"/>
+                    <img  class={props.photoFade==null || props.photoFade==0 ? "slideImg fade" : "slideImg fade2"} src={props.currentImg} alt="imgOne"/> 
                     <a class="next" onClick={props.plusSlide}>&#10095;</a>
+                    </div>
+                   
+                   
                    
                 </div>
                 </div>
